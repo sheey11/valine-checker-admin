a80c4b34f8da1a1f6e76e1480f6bb80ec7ebd429
Sheey <1511956322@qq.com>
2020-03-15 12:11:35 +0800

    :bug: fixed: public folder dose not exsits

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 cd pages
 npm run build --name "Valine Checker"
 cd ..
+if [ ! -d "server/public/" ];then
+    mkdir server/public/
+fi
 cp -r pages/dist/* server/public/
--- build.yarn.sh
+++ build.yarn.sh
@@ -2,4 +2,7 @@
 cd pages
 yarn build --name "Valine Checker"
 cd ..
+if [ ! -d "server/public/" ];then
+    mkdir server/public/
+fi
 cp -r pages/dist/* server/public/
